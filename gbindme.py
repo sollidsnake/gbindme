d0bdb0cb3a349a170d6710307d884085e9e6b477
Jesse Nazario <jessenzr@gmail.com>
2014-05-07 09:10:47 -0300

    use the new gobject instead of pygtk

--- gbindme.py
+++ gbindme.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python2
-import pygtk
-pygtk.require('2.0')
-import gtk, appindicator, keybinder
-import os, sys
+#!/usr/bin/env python3
+
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Keybinder', '3.0')
+from gi.repository import Keybinder as keybinder
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+import os, subprocess
 import lib.singleton
-import ConfigParser
+import configparser
 
 class Ui(object):
     def delete_event(self, widget, event, data=None):
         return False
 
     def hello(self, widget, data=None):
-        print "hun?"
+        print("hun?")
 
     def settings(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
@@ -26,24 +31,21 @@ class Ui(object):
         #self.button.show()
         self.window.show()
 
-
-        #keystr = "o"
-        #keybinder.bind(keystr, self.callback, "nohup gmrun >> /tmp/pyhk.log &")
-
 class Indicator(object):
     def menuitem_response(self, w, buf):
         function = {
                 'exit': sys.exit,
                 'settings': self.settings,
-                'reload': self.handleKeys
+                'reload': self.handleKeys,
+                'openkeys.cfg': self.openKeysCfg
         }[buf]()
         pass
 
     def startIndicator(self):
-        self.ind = appindicator.Indicator("example-simple-client",
+        self.ind = appindicator.Indicator.new("example-simple-client",
                 "apple-green",
-                appindicator.CATEGORY_APPLICATION_STATUS)
-        self.ind.set_status (appindicator.STATUS_ACTIVE)
+                appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.ind.set_attention_icon("indicator-messages-new")
 
         menu = gtk.Menu()
@@ -55,6 +57,11 @@ class Indicator(object):
 
         menu_items = gtk.MenuItem('Reload')
         menu_items.connect("activate", self.menuitem_response, "reload")
+        menu.append(menu_items)
+        menu_items.show()
+
+        menu_items = gtk.MenuItem("Open keys.cfg")
+        menu_items.connect("activate", self.menuitem_response, "openkeys.cfg")
         menu.append(menu_items)
         menu_items.show()
 
@@ -71,7 +78,7 @@ class Hk(Indicator, Ui):
 
     def __init__(self):
         self.cfgDir = os.path.expanduser('~') \
-                + '/.config//'
+                + '/.config/' + self.nameLower + '/'
         if not os.path.exists(self.cfgDir):
             os.makedirs(self.cfgDir)
 
@@ -83,11 +90,15 @@ class Hk(Indicator, Ui):
         if not os.path.isfile(self.keyCfg):
             open(self.keyCfg, 'a').close()
 
-    
-    def callback(self, user_data):
-        os.system(user_data)
+    def callback(self, user_data, cmd):
+        cmd = 'nohup ' + cmd + '&'
+        os.system(cmd)
+
+    def openKeysCfg(self):
+        subprocess.call(['xdg-open', self.keyCfg])
 
     def run(self):
+        keybinder.init()
         me = lib.singleton.SingleInstance()
         self.handleKeys()
         self.startIndicator()
@@ -96,20 +107,21 @@ class Hk(Indicator, Ui):
     def handleKeys(self):
         if len(self.boundKeys)>0:
             for key in self.boundKeys:
-                keybinder.unbind(key)
+                #keybinder.unbind(key)
+                pass
             self.boundKeys = []
 
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read(self.keyCfg)
         sections = config.sections()
 
         for s in sections:
-            cmd = config.get(s, 'Exec')
+            self.cmd = cmd = config.get(s, 'Exec')
             key = config.get(s, 'Key')
             try:
-                keybinder.bind(key, self.callback, "nohup " + cmd)
+                keybinder.bind(key, self.callback, cmd)
                 self.boundKeys.append(key)
             except:
-                print "Error: can't bind ", key, "key"
+                print("Error: can't bind ", key, "key")
 
 Hk().run()
